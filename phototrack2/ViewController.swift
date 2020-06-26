8cd83a048df66fd94bed3b495b2ac515a53b11d5
Amit Biswas <biswas1992@hotmail.com>
2020-06-26 19:16:46 -0400

    Initial Commit

---
+++ phototrack2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phototrack2
+//
+//  Created by Amit Biswas on 6/26/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
